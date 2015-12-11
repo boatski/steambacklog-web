8b43c607b39f23c8f92d292b1b3b20eb3ec90169
Chad Strauch <boatski313@gmail.com>
2015-12-11 00:40:47 -0500

    messing w/ cards

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,10 @@
 import {bootstrap, Component} from 'angular2/angular2';
 import {Header} from './header/header.ts';
+import {Home} from './main/home/home.ts';
 
 @Component({
     selector: 'app',
-    directives: [Header],
+    directives: [Header, Home],
     templateUrl: 'app/app.html'
 })
 class AppComponent { }
---
+++ app/main/home/home.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'home',
+    templateUrl: 'app/main/home/home.html'
+})
+export class Home { }
