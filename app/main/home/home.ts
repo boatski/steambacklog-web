eb28e01287e8cc9d0b6912963db2c56feb0f3e7e
Chad Strauch <boatski313@gmail.com>
2015-12-14 23:42:54 -0500

    rxjs shenanigans, resolve vanity fixed on click

--- app/main/home/home.ts
+++ app/main/home/home.ts
@@ -1,7 +1,25 @@
 import {Component} from 'angular2/angular2';
+import {FORM_DIRECTIVES} from 'angular2/angular2';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-    selector: 'home',
-    templateUrl: 'app/main/home/home.html'
+  selector: 'home',
+  viewProviders: [HTTP_PROVIDERS],
+  templateUrl: 'app/main/home/home.html'
 })
-export class Home { }
+export class Home {
+  constructor(public http: Http) {
+
+  }
+  resolveUrl($event) {
+    var result;
+    console.log('resolving id: ' + 'http://127.0.0.1:3000/resolve/' + $event);
+    this.http.get('http://127.0.0.1:3000/resolve/' + $event)
+    .map(response => response.json())
+    .subscribe(
+      response => console.log(response),
+      () => console.log('Resolved Vanity Url')
+    );
+  }
+}
